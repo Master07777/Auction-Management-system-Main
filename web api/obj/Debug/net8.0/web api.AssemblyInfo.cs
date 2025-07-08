66406fd9059efc213d3395badbfa0929913aa591
Yadav <2417825@cognizant.com>
2025-07-08 09:31:16 +0530

    work

--- web api/obj/Debug/net8.0/web api.AssemblyInfo.cs
+++ web api/obj/Debug/net8.0/web api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+732d75f8fdb86e38d52e0a50c6d7a07a239d5843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46207f2f8fac388b77eecb74cb26a7cd399cd63")]
 [assembly: System.Reflection.AssemblyProductAttribute("web api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
